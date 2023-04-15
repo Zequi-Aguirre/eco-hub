e1bdb9e10973e295a7b61310bfb938d051a19202
Julia Dannerth <jdannerth@gmail.com>
2023-04-15 11:39:08 -0400

    landing page updated

--- src/componenets/landing-page/LandingPage.js
+++ src/componenets/landing-page/LandingPage.js
@@ -42,21 +42,20 @@ export default function LandingPage() {
     <div className="video-bg">
     <div class="band"><h3 className="band-text">Click on a Species to learn more</h3></div>
       <video autoPlay muted loop className="video" src={video1}/>
-
-      <div id="button-container" className="buttons-container">
-        <Link to="/report-sightings" id = "button button-report-sightings">
+      
+      <div class="info-container">
+      <Link to="/report-sightings" id = "button button-report-sightings">
           <button className="button button-report-sightings">Report Sightings</button>
         </Link>
-        <br></br>
         <Link to="/informational" id = "button button-informational">
           <button className="button button-informational">Informational</button>
         </Link>
-        <br></br>
         <Link to="/map" id = "button button-map">
         <button className="button button-map">Map</button>
         </Link>
       </div>
-    </div>
+
+      </div>
   )
 }
 
--- src/componenets/map/Map.js
+++ src/componenets/map/Map.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import video4 from '../../assets/video/egret.mp4'
 
 export default function Map() {
   return (
-    <div>Map</div>
+    <div className="video-bg">
+    <div className="container">
+   <video autoPlay muted loop className="video" src={video4}/>
+   </div>
+   </div>
   )
 }
