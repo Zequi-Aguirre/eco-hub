670fbd737fbde4de785b53e6afabfa26ca708b8f
borbersaur <plasmabeach@gmail.com>
2023-04-15 13:29:24 -0400

    can upload image and get name of plant

--- src/componenets/report-sightings/ReportSightings.js
+++ src/componenets/report-sightings/ReportSightings.js
@@ -1,12 +1,56 @@
-import React from 'react'
-const video3 = "https://res.cloudinary.com/dacloudycloud/video/upload/v1681574607/snail.mp4"
+import React, {useState} from 'react';
+import axios from "axios";
+import ReactImageFileToBase64 from "react-file-image-to-base64";
+
+const video3 = "https://res.cloudinary.com/dacloudycloud/video/upload/v1681574607/snail.mp4";
+
+
 
 export default function ReportSightings() {
+  const [image, setImage] = useState({});
+  const [nameOfPLant, setNameOfPLant] = useState("");
+  
+  console.log(nameOfPLant);
+
+  const handleOnCompleted = files => {
+    console.log(files[0].base64_file)
+    setImage(files[0]);
+    runSampleApiCall(files[0].base64_file);
+  };
+
+const runSampleApiCall = (baseString) =>{
+  axios.post("https://api.plant.id/v2/identify", {images: [baseString]}, 
+  { headers: 
+    {
+    "Api-Key":"Vh4cdMbb2TcDKTATp10GoIys7WWOuI2PsrH6DdvEpvX9fPJZIh"
+    }
+  })
+  .then((response)=>{
+    if(response.data && response.data.suggestions){
+      console.log(response);
+      setNameOfPLant(response.data.suggestions[0].plant_name);
+    }
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+  
+}
+
+
+
+
   return (
-    <div className="video-bg">
-      <div className="container">
-     <video autoPlay muted loop className="video" src={video3}/>
-     </div>
-     </div>
+    <div>
+        <div>
+          <ReactImageFileToBase64 onCompleted={handleOnCompleted} />
+        </div>
+      <div className="video-bg">
+        <div className="container">
+      <video autoPlay muted loop className="video" src={video3}/>
+      </div>
+      </div>
+    </div>
+      
   )
 }
